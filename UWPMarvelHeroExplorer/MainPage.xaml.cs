e2d7e75c6125a56c6f482b52e995b72fe8e80a3d
kaamraan <kaamraank@outlook.com>
2017-02-02 20:08:06 +0530

    Add app demo gif

--- UWPMarvelHeroExplorer/MainPage.xaml.cs
+++ UWPMarvelHeroExplorer/MainPage.xaml.cs
@@ -47,6 +47,7 @@ namespace UWPMarvelHeroExplorer
                 MainFrame.Navigate(typeof(AboutPage));
                 HeadingTextBlock.Text = "About";
             }
+            MainSplitView.IsPaneOpen = false;
         }
 
         private void Page_Loaded(object sender, RoutedEventArgs e)
@@ -54,92 +55,5 @@ namespace UWPMarvelHeroExplorer
             HeadingTextBlock.Text = "Home";
             MainFrame.Navigate(typeof(Homepage));
         }
-
-
-
-
-
-        //public ObservableCollection<Character> Characters;
-        //public ObservableCollection<Comic> Comics;
-
-        //private BitmapImage CurrentImage;
-
-        //public MainPage()
-        //{
-        //    this.InitializeComponent();
-        //    Characters = new ObservableCollection<Character>();
-        //    Comics = new ObservableCollection<Comic>();
-        //    CurrentImage = new BitmapImage();
-        //}
-
-        //private async void Page_Loaded(object sender, RoutedEventArgs e)
-        //{
-        //    DetailsGrid.Visibility = Visibility.Collapsed;
-
-        //    MainProgressRing.IsActive = true;
-        //    MainProgressRing.Visibility = Visibility.Visible;
-
-        //    try {
-        //        while (Characters.Count < 20) {
-        //            await MarvelFacade.UpdateCharacterListAsync(Characters);
-        //        }
-        //    }
-        //    catch (Exception) {
-
-        //    }
-
-        //    MainProgressRing.IsActive = false;
-        //    MainProgressRing.Visibility = Visibility.Collapsed;
-
-        //    AttributeTextBlock.Visibility = Visibility.Visible;
-        //    ListHeadingTextBlock.Visibility = Visibility.Visible;
-        //}
-
-        //private async void CharactersListView_ItemClick(object sender, ItemClickEventArgs e)
-        //{
-        //    if (DetailsGrid.Visibility == Visibility.Collapsed) DetailsGrid.Visibility = Visibility.Visible;
-
-        //    MainProgressRing.IsActive = true;
-        //    MainProgressRing.Visibility = Visibility.Visible;
-
-        //    var selectedCharacter = e.ClickedItem as Character;
-
-        //    var imageSource = new BitmapImage();
-        //    var imageLocation = new Uri(selectedCharacter.thumbnail.large, UriKind.Absolute);
-        //    imageSource.UriSource = imageLocation;
-
-        //    CurrentImage = imageSource;
-
-        //    CharacterDetailsImage.Source = imageSource;
-
-        //    CharacterNameTextBlock.Text = selectedCharacter.name;
-        //    DescriptionTextBlock.Text = selectedCharacter.description;
-
-        //    await MarvelFacade.UpdateComicListAsync(Comics, selectedCharacter.id);
-
-        //    ImageGrid.Visibility = Visibility.Visible;
-        //    FeaturedTextBlock.Visibility = Visibility.Visible;
-
-        //    MainProgressRing.IsActive = false;
-        //    MainProgressRing.Visibility = Visibility.Collapsed;
-        //}
-
-        //private void CharacterDetailsImage_Tapped(object sender, TappedRoutedEventArgs e)
-        //{
-        //    FlyoutImage.Source = CurrentImage;
-        //    FlyoutBase.ShowAttachedFlyout((FrameworkElement)sender);
-        //    MainGridBlurStoryboard.Begin();
-        //}
-
-        //private void CharacterImageFlyout_Closed(object sender, object e)
-        //{
-        //    MainGridShowStoryboard.Begin();
-        //}
-
-        //private void ComicImage_Tapped(object sender, TappedRoutedEventArgs e)
-        //{
-        //    FlyoutBase.ShowAttachedFlyout((FrameworkElement)sender);
-        //    MainGridBlurStoryboard.Begin();
-        //}
     }
 }
